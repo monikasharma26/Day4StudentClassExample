6e39d691a60d352ad24e0199df4b0f9e1c4a1040
MacStudent <macstudent@128-IMAC27.local>
2020-01-27 20:02:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MAD3463_Java_Sample
+
+## Pritesh Patel
+
+---
+Day 4 
+
+Reference Link
+
+- https://git-scm.com/docs
+- https://www.w3schools.com/JAVA/java_date.asp
+- https://www.journaldev.com/7153/core-java-tutorial
+- https://www.tutorialspoint.com/java/java_object_classes.htm
+
+# Date: Friday, 24th January, 2020
---
+++ src/main/java/com/lambton/StudentMainClass.java
@@ -0,0 +1,39 @@
+package com.lambton;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class StudentMainClass
+{
+    public static void main(String[] args) {
+
+        Student[] listOfStudents = new Student[2];
+
+        //First Student Object
+        LocalDate s1BirthDate =  LocalDate.of(1981, Month.FEBRUARY, 20);
+        Student s1 = new Student(1,"Charmi","Patel", s1BirthDate ,Gender.FEMALE,
+                new float[]{50.0f, 20.0f, 40, 70, 60});
+
+       listOfStudents[0] = s1;
+
+        //NullPointerException
+        // listOfStudents[0].printData();
+
+        //Second Student Object
+        LocalDate s2BirthDate =  LocalDate.of(1992, Month.MARCH, 25);
+        Student s2 = new Student(2,"Pritesh","Patel", s2BirthDate ,Gender.MALE,
+                new float[]{50.0f, 80.0f, 50, 70, 60});
+
+       listOfStudents[1] = s2;
+
+       for(Student s: listOfStudents)
+       {
+           s.calculateTotalMarks();
+           s.calculatePercentage();
+           s.calculateResult();
+           s.printData();
+       }
+    }
+}
+
+
